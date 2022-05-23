6cf2a1b3913696d79f497ae61295432d2fb67bd8
unknown <dbasedown211@gmail.com>
2022-05-23 16:42:45 +0700

    day 1

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -17,13 +17,70 @@
     <script type="text/babel">
         const root = document.querySelector('#root');
 
-        const element = (
-            <ul>
-                <li>Apple</li>
-                <li>Banana</li>
-                <li>Orange</li>
-            </ul>
-        );
+
+
+
+        ReactDOM.render(element, root);
+
+
+
+
+        // function tick() {
+        //     const element = (
+        //         <div>
+        //             <h1>Jam Sekarang</h1>
+        //             <h2>{new Date().toLocaleTimeString()}</h2>
+        //         </div>
+        //     )
+        // ReactDOM.render(element, root);
+        // }
+        // tick();
+        // setInterval(function() {
+        //     tick();
+        // }, 1000);
+
+        // function Halo(props) {
+        //     return <p>Halo <b>{props.name}</b></p>
+        // }
+
+        // const element = (
+        //     <>
+        //         <Halo name="Haha" />
+        //         <Halo name="Hihi" />
+        //         <Halo name="Hoho" />
+        //     </>
+        // );
+
+        // function halo(name) {
+        //     return <p>Halo <b>{name}</b></p>
+        // }
+
+        // const element = (
+        //     <>
+        //         {halo('Haha')}
+        //         {halo('Hihi')}
+        //         {halo('Hoho')}
+        //     </>
+        // );
+
+        // const element = <img src="https://images.pexels.com/photos/753626/pexels-photo-753626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+        // const element = <h1 className = "heading-1">Hello Lyzer</h1>
+
+        // function ngegas(text) {
+        //     return text.toUpperCase();
+        // }
+
+        // const name = 'Lyzer';
+        // const element =<h1>Hello {ngegas(name)}</h1>
+
+        // (
+        //     <ul>
+        //         <li>Apple</li>
+        //         <li>Banana</li>
+        //         <li>Orange</li>
+        //     </ul>
+        // );
 
         // document.createElement('h1');
         // element.textContent = 'Hello guys';
@@ -31,7 +88,7 @@
 
         // root.appendChild(element);
 
-        ReactDOM.render(element, root);
+        // ReactDOM.render(element, root);
     </script>
 
 </body>
