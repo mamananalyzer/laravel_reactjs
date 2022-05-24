35a6944aa3bb121ffb33d069e135e524d5822f45
unknown <dbasedown211@gmail.com>
2022-05-24 09:18:49 +0700

    day 2

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 Route::get('/next', function () {
     return view('layout.project');
